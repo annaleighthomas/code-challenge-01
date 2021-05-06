be71b537eb3d22ae98ee730f4429acca128a6bdc
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-05 22:28:15 -0400

    passing more object keys entries test

--- src/more-object-keys-entries.js
+++ src/more-object-keys-entries.js
@@ -12,16 +12,49 @@ export function getHouses(characters) {
 
 
 export function updateNumbers(obj) {
-
+  const arr = [];
+  Object.keys(obj).forEach(key => {
+    arr.push(`${key}: ${obj[key]}`);
+  });
+  return arr;
 }
 
-// export function totalCharacters(arr) {
+export function totalCharacters(arr) {
+  let characters = 0;
 
-// }
+  arr.forEach(family => {
+    Object.keys(family).forEach(property => {
 
-// export function hasChildrenEntries(arr, character) {
+      const value = family[property];
 
-// } 
+      if (property !== 'house' && property !== 'children') {
 
-// export function sortByChildren(arr){
-// } 
+        if (value) characters++;
+      }
+      if (property === 'children') value.forEach(child => characters++);
+    });
+  });
+
+  return characters;
+}
+
+export function hasChildrenEntries(arr, character) {
+  let children = 0;
+  Object.entries(arr).forEach(family => {
+    family.forEach(person => {
+      if (person.name === character) {
+        children = person.children.length > 0 ? true : false;
+      }
+    });
+  });
+  return children;
+}
+
+
+export function sortByChildren(arr){
+  return arr.sort((a, b) => {
+    const childrenLength = a.children.length - b.children.length;
+    if (childrenLength !== 0) return childrenLength;
+    return (a.name < b.name);
+  });
+} 
