c633ba2fe7c3fcc75e9a11e77d0df025de4f1db0
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-07 17:50:34 -0400

    added array method setup

---
+++ src/array-methods.js
@@ -0,0 +1,31 @@
+export function howMuchPencil(str) {
+    
+}
+
+// export function wordsToCharList(arr) {
+    
+// }
+
+// export function stepActions(recipe){
+  
+// }
+
+// export function removeLastCharacters(str, numberOfCharacters){
+  
+// }
+
+// export function totalSumCSV(str){
+  
+// }
+
+// export function removeVowels(str){
+  
+// }
+
+// export function removeVowels(str){
+  
+// }
+
+// export function extractVowels(str){
+  
+// }
