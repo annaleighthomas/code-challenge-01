66b5ee6e588773f3ebb339da35789853cc3b3e3c
Annaleigh Thomas <annaleighxvx@gmail.com>
2021-05-13 11:22:44 -0400

    sortByCHhildren func

--- src/code-challenge-13.js
+++ src/code-challenge-13.js
@@ -1,3 +1,7 @@
 export function sortByChildren(array) {
-
+  return array.sort((a, b) => {
+    const childrenLength = a.children.length - b.children.length;
+    if (childrenLength !== 0) return childrenLength;
+    return (a.name < b.name);
+  });
 }
